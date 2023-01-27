2445b8f68957a61f00e5598f792226fb8877b91f
unknown <leonenazareno@gmail.com>
2023-01-27 17:58:59 -0300

    primer commit

---
+++ dir/enviar.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['nombre'];
+$tel = $_POST['tel'];
+$correo = $_POST['correo'];
+$msj = $_POST['msj'];
+
+$header = "From: " . $correo . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$msj = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$msj .= "Su e-mail es: " . $correo . " \r\n";
+$msj .= "Teléfono de contacto: " . $tel . " \r\n";
+$msj .= "Mensaje: " . $_POST['msj'] . " \r\n";
+$msj .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'leonenazareno@gmail.com';
+$asunto = 'Asunto del mensaje';
+
+mail($para, $asunto, utf8_decode($msj), $header);
+
+header("Location:index.html");
+?>
